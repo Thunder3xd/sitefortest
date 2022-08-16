29dd3fc1e69f4f9ed9fc1d5a993ba80752863774
Thunder3xd <vladungureanu369@gmail.com>
2022-08-16 12:14:30 +0300

    add basic loyaut

---
+++ siteblog/blog/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+
+from .views import *
+
+
+urlpatterns = [
+    path('', Home.as_view(), name='home'),
+    path('unique/', Unique.as_view(), name='unique'),
+    path('category/<str:slug>/', PostsByCategory.as_view(), name='category'),
+    path('tag/<str:slug>/', PostsByTag.as_view(), name='tag'),
+    path('post/<str:slug>/', GetPost.as_view(), name='post'),
+    path('search/', Search.as_view(), name='search'),
+]
